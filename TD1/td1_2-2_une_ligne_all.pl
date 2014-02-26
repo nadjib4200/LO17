6bf9663353e9759c3c06dd43c1dffb967d77519b
Tudor Luchiancenco <tudorluchy@gmail.com>
2014-02-26 15:22:36 +0100

    td1 une ligne

--- TD1/td1_2-2_une_ligne_all.pl
+++ TD1/td1_2-2_une_ligne_all.pl
@@ -1,20 +1,20 @@
 #!/usr/bin/perl
 
 # recupération tous les fichiers HTML dans un tableau
-@list = `ls LCI/*.html`;
+@list = `ls LCI_2/*.html`;
 $nb_fic = @list;
 print "Nombre de fichiers originaux : $nb_fic\n";
 
-# on enlève le "LCI/" du nom
+# on enlève le "LCI_2/" du nom
 foreach $fic (@list) {
-  $fic =~ s/LCI\///;
+  $fic =~ s/LCI_2\///;
 }
 
 # fichiers générées
 $i = 0;
-# pour chaque fichier on appelle le script td1_2-1_une.pl
+# pour chaque fichier on appelle le script td1_2-2_une_ligne.pl
 foreach $fic (@list) {
-  `perl td1_2-1_une.pl $fic`; 
+  `perl td1_2-2_une_ligne.pl $fic`; 
    $i++;
 }
 
