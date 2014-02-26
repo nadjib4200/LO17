a4c85aecc0b772099e127a82d4f8aecfd44461df
Tudor Luchiancenco <tudorluchy@gmail.com>
2014-02-26 15:02:05 +0100

    td1 init suite

---
+++ TD1/td1_2-2_une_ligne.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+# ex : script.pl nom_fic
+# script doit être à coté de LCI
+
+# (1) quit unless we have the correct number of command-line args
+$num_args = $#ARGV + 1;
+print "Nb args : $num_args\n";
+if ($num_args != 1) {
+	print "Usage: script.pl nom_fic\n";
+	exit;
+}
+
+# lecture args
+$fichier = $ARGV[0];
+
+print "Source : LCI_2/$fichier\n";
+
+open(FICIN, "LCI_2/$fichier") || die ("Erreur d'ouverture du fichier");
+open(FICOUT, ">LCI_3/$fichier") || die ("Erreur d'ouverture du fichier");
+
+# lecture fichier entrée
+while ($ligne = <FICIN>) {
+	if ($ligne =~ s/(\n|\r|\f)//g) {
+		print FICOUT $ligne;
+	}
+}
+
+print "END\n";
+
+
